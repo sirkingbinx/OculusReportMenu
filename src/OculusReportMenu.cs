c85124a7d1fad68e6a7ade45d823dd0ceeea9a3b
King Bingus <bendawsdev@gmail.com>
2025-05-05 09:57:45 -0500

    idk

--- src/OculusReportMenu.cs
+++ src/OculusReportMenu.cs
@@ -2,8 +2,6 @@
 // (C) Copyright 2024 - 2025 binx
 // MIT License
 
-// #define Cardboard
-
 using BepInEx;
 
 using HarmonyLib;
@@ -14,13 +12,10 @@ using UnityEngine.XR;
 using GorillaNetworking;
 using GorillaLocomotion;
 using BepInEx.Configuration;
-
 using Valve.VR;
 
 using OculusReportMenu.Patches;
 
-
-#if (!Cardboard)
 namespace OculusReportMenu {
     [BepInPlugin("kingbingus.oculusreportmenu", "OculusReportMenu", "1.2.1")]
     public class Plugin : BaseUnityPlugin
@@ -65,7 +60,7 @@ namespace OculusReportMenu {
             else if (GetControllerPressed() && ModEnabled) { ShowMenu(); }
         }
 
-        internal bool GetControllerPressed() => (CheckButtonPressedStatus(OpenButton1) && CheckButtonPressedStatus(OpenButton2)) | Keyboard.current.tabKey.wasPressedThisFrame;
+        internal sbool GetControllerPressed() => (CheckButtonPressedStatus(OpenButton1) && CheckButtonPressedStatus(OpenButton2)) | Keyboard.current.tabKey.wasPressedThisFrame;
 
         internal static void ShowMenu()
         {
@@ -79,8 +74,10 @@ namespace OculusReportMenu {
             }
         }
 
-        public void OnEnable() { ModEnabled = true; HarmonyPatches.ApplyHarmonyPatches(this); }
-        public void OnDisable() { ModEnabled = false; HarmonyPatches.RemoveHarmonyPatches(); }
+        public void Start() => HarmonyPatches.ApplyHarmonyPatches(this);
+
+        public void OnEnable() => ModEnabled = true;
+        public void OnDisable() => ModEnabled = false;
 
         void Awake()
         {
@@ -167,136 +164,3 @@ namespace OculusReportMenu {
         static void CheckPlatform() => Plugin.usingSteamVR = PlayFabAuthenticator.instance.platform.PlatformTag.ToLower().Contains("steam");
     }
 }
-#endif
-
-#if (Cardboard)
-using Cardboard;
-using Cardboard.Utils;
-using Input = Cardboard.Utils.Input;
-
-namespace OculusReportMenu
-{
-    [BepInDependency("kingbingus.cardboard")]
-    [BepInPlugin("kingbingus.oculusreportmenu", "OculusReportMenu", "1.2.1")]
-    public class Plugin : BaseUnityPlugin
-    {
-        // custom stuff
-        internal static ConfigEntry<string> OpenButton1, OpenButton2;
-
-        // base things
-        internal static bool Menu, ModEnabled;
-        internal static GorillaMetaReport MetaReportMenu;
-
-        internal static MethodInfo CheckDistance, CheckReportSubmit;
-
-        internal static GameObject occluder, metaLeftHand, metaRightHand;
-
-        void Update()
-        {
-            CheckDistance = typeof(GorillaMetaReport).GetMethod("CheckDistance", BindingFlags.NonPublic | BindingFlags.Instance);
-            CheckReportSubmit = typeof(GorillaMetaReport).GetMethod("CheckReportSubmit", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            if (Menu)
-            {
-                // hide the fact that they're in report menu to prevent comp cheating
-                GTPlayer.Instance.disableMovement = false;
-                GTPlayer.Instance.inOverlay = false;
-
-                // update stuff
-                occluder.transform.position = GorillaTagger.Instance.mainCamera.transform.position;
-
-                metaLeftHand.transform.SetPositionAndRotation(GorillaTagger.Instance.offlineVRRig.leftHandTransform.position, GorillaTagger.Instance.offlineVRRig.leftHandTransform.rotation);
-                metaRightHand.transform.SetPositionAndRotation(GorillaTagger.Instance.offlineVRRig.rightHandTransform.position, GorillaTagger.Instance.offlineVRRig.rightHandTransform.rotation); ;
-
-                CheckDistance.Invoke(MetaReportMenu, null);
-                CheckReportSubmit.Invoke(MetaReportMenu, null);
-            }
-            else if (GetControllerPressed() && ModEnabled) { ShowMenu(); }
-        }
-
-        internal bool GetControllerPressed() => (CheckButtonPressedStatus(OpenButton1) && CheckButtonPressedStatus(OpenButton2)) | Keyboard.current.tabKey.wasPressedThisFrame;
-
-        internal static void ShowMenu()
-        {
-            if (!Menu)
-            {
-                MetaReportMenu.gameObject.SetActive(true);
-                MetaReportMenu.enabled = true;
-
-                typeof(GorillaMetaReport).GetMethod("StartOverlay", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(MetaReportMenu, null);
-                Menu = true;
-            }
-        }
-
-        public void OnEnable() { ModEnabled = true; CardboardHarmony.PatchInstance(this); }
-        public void OnDisable() { ModEnabled = false; CardboardHarmony.UnpatchInstance(); }
-
-        void Awake()
-        {
-            OpenButton1 = Config.Bind("Keybinds",
-                                      "OpenButton1",
-                                      "LS",
-                                      "One of the buttons you use to open ORM (NAN for none)");
-
-            OpenButton2 = Config.Bind("Keybinds",
-                                      "OpenButton2",
-                                      "RJ",
-                                      "One of the buttons you use to open ORM (NAN for none)");
-        }
-
-        // checks for the right key
-
-        internal static bool CheckButtonPressedStatus(ConfigEntry<string> thisEntry)
-        {
-            switch (thisEntry.Value.ToUpper())
-            {
-                // left hand
-                case "LP": return Input.GetValue(InputType.leftPrimary);
-                case "LS": return Input.GetValue(InputType.leftSecondary);
-                case "LT": return Input.GetValue(InputType.leftTrigger);
-                case "LG": return Input.GetValue(InputType.leftGrip);
-                case "LJ": return Input.GetValue(InputType.leftStick);
-
-                // right hand
-                case "RP": return Input.GetValue(InputType.rightPrimary);
-                case "RS": return Input.GetValue(InputType.rightSecondary);
-                case "RT": return Input.GetValue(InputType.rightTrigger);
-                case "RG": return Input.GetValue(InputType.rightGrip);
-                case "RJ": return Input.GetValue(InputType.rightStick);
-
-                case "NAN": return true;
-                default: return true;
-            }
-        }
-    }
-
-    public class GamePatches
-    {
-        /*
-         * hey AA devs plz don't patch OculusReportMenu i really like getting cheaters banned
-         * pls dm me on discord if you want this taken down i just want cheaters gone man
-         * 
-         * <3 sirkingbinx
-         */
-
-        [HarmonyPatch(typeof(GorillaMetaReport), "Start")]
-        static void StartPatch(GorillaMetaReport __instance)
-        {
-            Plugin.MetaReportMenu = __instance;
-
-            Plugin.CheckDistance = typeof(GorillaMetaReport).GetMethod("CheckDistance", BindingFlags.NonPublic | BindingFlags.Instance);
-            Plugin.CheckReportSubmit = typeof(GorillaMetaReport).GetMethod("CheckReportSubmit", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            Plugin.occluder = GameObject.Find("Miscellaneous Scripts/MetaReporting/ReportOccluder");
-            Plugin.metaLeftHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/LeftHandParent");
-            Plugin.metaRightHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/RightHandParent");
-        }
-
-        [HarmonyPatch(typeof(GorillaMetaReport), "Update")]
-        static void SteamPatch() => GTPlayer.Instance.InReportMenu = false;
-
-        [HarmonyPatch(typeof(GorillaMetaReport), "Teardown")]
-        static void MenuCloseHook() => Plugin.Menu = false;
-    }
-}
-#endif
