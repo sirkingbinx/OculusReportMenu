36d82700435cc11681a524f5ecaec0a7f3017fbf
Bingus <bendawsdev@gmail.com>
2025-05-27 11:40:21 -0500

    Fixed finally!

--- src/OculusReportMenu.cs
+++ src/OculusReportMenu.cs
@@ -19,6 +19,22 @@ using Valve.VR;
 using GorillaNetworking;
 using GorillaLocomotion;
 
+/*
+ * Dear Another Axiom,
+ * 
+ * Please stop patching this mod.
+ * As of right now, this is the only way to bypass anti-Oculus report on
+ * cheat menus such as ii's Stupid Menu, ShibaGT Gold, and others.
+ * 
+ * By patching this mod, you only commit more to making the cheater problem
+ * worse.
+ * 
+ * You can ban me from your servers or anything, just please leave this
+ * mod functional. (my username is @sirkingbinx if you are considering this)
+ * 
+ * - bingus
+ */
+
 namespace OculusReportMenu
 {
     [BepInPlugin("kingbingus.oculusreportmenu", "OculusReportMenu", "2.1.0")]
@@ -28,35 +44,25 @@ namespace OculusReportMenu
 
         internal GorillaMetaReport Menu;
         internal GameObject ORMOccluder, ORMLeftHand, ORMRightHand;
+
+        internal FieldInfo closeButton;
         internal MethodInfo UpdatePosition, CheckReports, OpenMenu, Teardown;
-        
-        internal bool ShowingMenu, ButtonsPressed, PlatformSteam;
+
+        internal bool ShowingMenu, ButtonsPressed;
 
         internal string OpenButton1, OpenButton2;
-        internal float Sensitivity;
-
-        // plugin
+        internal float Sensitivity, blockButtonsUntilTimestamp;
 
         internal void Start() {
-            Harmony.CreateAndPatchAll(GetType().Assembly, "kingbingus.oculusreportmenu");
+            Harmony.CreateAndPatchAll(GetType().Assembly, Info.Metadata.GUID);
+            instance = this;
 
             OpenButton1 = Config.Bind("Keybinds", "OpenButton1", "LS", "One of the buttons you use to open ORM (NAN for none)").Value;
-            OpenButton2 = Config.Bind("Keybinds", "OpenButton2", "RJ", "One of the buttons you use to open ORM (NAN for none)").Value;
+            OpenButton2 = Config.Bind("Keybinds", "OpenButton2", "RS", "One of the buttons you use to open ORM (NAN for none)").Value;
             Sensitivity = Config.Bind("Input", "Sensitivity", 0.5f, "Sensitivity of trigger / grip detection (0.5f = 50%)").Value;
 
             GorillaTagger.OnPlayerSpawned(delegate
             {
-                /*
-                i have no idea why this was the best way to do it, menu is now set in Harmony Patches
-                
-                    var metaReportMenu = Resources.FindObjectsOfTypeAll<GorillaMetaReport>();
-                    
-                    foreach (GorillaMetaReport m in metaReportMenu)
-                    {
-                        Menu = m;
-                    }
-                */
-            
                 ORMOccluder = GameObject.Find("Miscellaneous Scripts/MetaReporting/ReportOccluder");
                 ORMLeftHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/LeftHandParent");
                 ORMRightHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/RightHandParent");
@@ -70,11 +76,20 @@ namespace OculusReportMenu
                     BindingFlags.NonPublic | BindingFlags.Instance);
                 Teardown = typeof(GorillaMetaReport).GetMethod("CheckReportSubmit",
                     BindingFlags.NonPublic | BindingFlags.Instance);
+                closeButton = typeof(GorillaMetaReport).GetField("closeButton",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
             });
         }
 
         internal void Update() {
-            if (!Menu) {
+            blockButtonsUntilTimestamp = (float)
+                typeof(GorillaMetaReport).GetField("blockButtonsUntilTimestamp",
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Menu);
+
+            if (blockButtonsUntilTimestamp > Time.time)
+                return;
+
+            if (Menu != null) {
                 ButtonsPressed =
                     (CheckButtonPressedStatus(OpenButton1) && CheckButtonPressedStatus(OpenButton2))
                     | Keyboard.current.tabKey.wasPressedThisFrame;
@@ -82,6 +97,7 @@ namespace OculusReportMenu
                 if (ShowingMenu) {
                     GTPlayer.Instance.disableMovement = false;
                     GTPlayer.Instance.inOverlay = false;
+                    GTPlayer.Instance.InReportMenu = false;
 
                     ORMOccluder.transform.position = GorillaTagger.Instance.mainCamera.transform.position;
 
@@ -96,54 +112,41 @@ namespace OculusReportMenu
                     CheckReports.Invoke(Menu, null);
                 } else if (ButtonsPressed)
                 {
-                    object[] args = { true };
-
                     Menu.gameObject.SetActive(true);
                     Menu.enabled = true;
 
-                    OpenMenu.Invoke(Menu, args);
+                    object[] stuff = { false };
+                    OpenMenu.Invoke(Menu, stuff);
                     ShowingMenu = true;
                 }
             }
 
             if (!Menu.gameObject.activeInHierarchy && ShowingMenu)
                 ShowingMenu = false;
+
+            if (((GorillaReportButton)closeButton.GetValue(Menu)).selected)
+                Teardown.Invoke(Menu, null);
         }
 
         internal bool CheckButtonPressedStatus(string thisEntry)
         {
-            bool temporarySClick = false;
-
             switch (thisEntry.ToUpper())
             {
                 case "LP": return ControllerInputPoller.instance.leftControllerPrimaryButton;
                 case "LS": return ControllerInputPoller.instance.leftControllerSecondaryButton;
                 case "LT": return ControllerInputPoller.instance.leftControllerIndexFloat > Sensitivity;
                 case "LG": return ControllerInputPoller.instance.leftControllerGripFloat > Sensitivity;
-                case "LJ":
-                    if (PlatformSteam)
-                        temporarySClick = SteamVR_Actions.gorillaTag_LeftJoystickClick.state;
-                    else
-                        InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxisClick, out temporarySClick);
-
-                    return temporarySClick;
+                case "LJ": return ControllerInputPoller.instance.leftControllerSecondaryButton;
 
                 // right hand
                 case "RP": return ControllerInputPoller.instance.rightControllerPrimaryButton;
                 case "RS": return ControllerInputPoller.instance.rightControllerSecondaryButton;
                 case "RT": return ControllerInputPoller.instance.rightControllerIndexFloat > Sensitivity;
                 case "RG": return ControllerInputPoller.instance.rightControllerGripFloat > Sensitivity;
-                case "RJ":
-                    if (PlatformSteam)
-                        temporarySClick = SteamVR_Actions.gorillaTag_RightJoystickClick.state;
-                    else
-                        InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxisClick, out temporarySClick);
-
-                    return temporarySClick;
+                case "RJ": return ControllerInputPoller.instance.rightControllerSecondaryButton;
 
                 // NAN
-                case "NAN":
-                    return true;
+                case "NAN": return true;
             }
             
             return false;
@@ -159,18 +162,20 @@ namespace OculusReportMenu
         [HarmonyPatch(typeof(GorillaMetaReport), "Start")]
         internal class OnReportInit
         {
-            static void Postfix(GorillaMetaReport __instance) => Menu = __instance;
+            static void Postfix(GorillaMetaReport __instance) => instance.Menu = __instance;
         }
 
-        // GorillaComputer
-        [HarmonyPatch(typeof(GorillaComputer), "Initialise")]
-        internal class OnComputerInit
+        [HarmonyPatch(typeof(GorillaMetaReport), "Update")]
+        internal class StopUpdate
         {
-            static void Postfix() =>
-                instance.PlatformSteam =
-                    PlayFabAuthenticator.instance.platform.PlatformTag
-                    .ToLower()
-                    .Contains("steam");
+            static void Prefix() { return; }
+        }
+
+        [HarmonyPatch(typeof(GorillaMetaReport), "StartOverlay")]
+        internal class LetGGWPCook
+        {
+            // This is necessary because the GGWP moderation uses the report menu to show stuff. This should make sure we don't make your game break; don't change it plz
+            static void Postfix() => instance.ShowingMenu = true;
         }
     }
 }
