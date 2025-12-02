37ad26c2ce4b15733144b9ce4ab4e88ff9fe82b8
Bingus of the Forest <bendawsdev@gmail.com>
2025-12-02 13:04:21 -0600

    basic project setup

--- Main.cs
+++ Main.cs
@@ -3,113 +3,69 @@
 // MIT License
 
 /*
- * TODO: This may be a terrible idea but I want to bind the report menu to Link's "Report Abuse" button
- *       May be a little strange but this would make opening the report menu super straight forward
- *       and feel like an actual intentional feature
- *       (Still leave keybinds, but give them another way to do it)
- */
+ * NOTE: I have the mod currently laid out how I want it, but it is still not functional (yet).
+ *	 The rest will come later, be patient
+*/
 
 using BepInEx;
 using GorillaLocomotion;
-using GorillaNetworking;
 using HarmonyLib;
 using UnityEngine;
 
-namespace OculusReportMenu
+namespace OculusReportMenu;
+
+[BepInPlugin("bingus.oculusreportmenu", "OculusReportMenu", "3.0.0")]
+public class Main : BaseUnityPlugin
 {
-    [BepInPlugin("bingus.oculusreportmenu", "OculusReportMenu", "2.2.5")]
-    internal class Plugin : BaseUnityPlugin
-    {
-        private static GorillaMetaReport _menu;
+    public static Main Instance { get; private set; }
 
-        private static bool __menuInit = false;
-        private static bool _menuInit {
-            get {
-                if (!__menuInit)
-                    __menuInit == (_menu != null);
-                
-                return __menuInit;
-            };
-            
-            set => __menuInit = value; // just in case
-        }
-        
-        private GameObject _occluder, _leftHand, _rightHand;
-        private bool _showingMenu, _platformSteam;
+    public bool Initialized;
 
-        internal void Start() {
-            Harmony.CreateAndPatchAll(GetType().Assembly, Info.Metadata.GUID);
-
-            Input.UseCustomKeybinds = Config.Bind("Keybinds",
-                "UseCustomKeybinds", true,
-                "Use your custom keybind settings (when off, press left + right secondaries)"
-            ).Value;
-
-            Input.AllowTabOpening = Config.Bind("Keybinds", "AllowTabOpen", false, "Press TAB to open").Value;
-
-            Input.OpenButton1 = Config.Bind("Keybinds", "OpenButton1", "LS",
-                "One of the buttons you use to open ORM (NAN for none)").Value;
-            Input.OpenButton2 = Config.Bind("Keybinds", "OpenButton2", "RS",
-                "One of the buttons you use to open ORM (NAN for none)").Value;
-            Input.Sensitivity = Config.Bind("Keybinds", "Sensitivity", 0.5f,
-                "Sensitivity of trigger / grip detection (0.5f = 50%)").Value;
-
-            GorillaTagger.OnPlayerSpawned(delegate
-            {
-                try {
-                    _occluder = GameObject.Find("Miscellaneous Scripts/MetaReporting/ReportOccluder");
-                    _leftHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/LeftHandParent");
-                    _rightHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/RightHandParent");
-                    
-                    _platformSteam = PlayFabAuthenticator.instance.platform.PlatformTag.ToLower().Contains("steam");
-                } catch (var ex) {
-                    Debug.Log($"Failed to load OculusReportMenu: ${ex}");
-                }
-            });
-        }
-
-        internal void Update() {
-            if (_menuInit)
-            {
-                if (_menu.blockButtonsUntilTimestamp > Time.time)
-                    return;
-
-                if (_showingMenu) {
-                    GTPlayer.Instance.disableMovement = false;
-                    GTPlayer.Instance.inOverlay = false;
-                    GTPlayer.Instance.InReportMenu = false;
-                    
-                    _occluder.transform.position = GorillaTagger.Instance.mainCamera.transform.position;
-
-                    _rightHand.transform.SetPositionAndRotation(
-                        GTPlayer.Instance.RightHand.controllerTransform.position,
-                        GTPlayer.Instance.RightHand.controllerTransform.rotation);
-
-                    _leftHand.transform.SetPositionAndRotation(
-                        GTPlayer.Instance.LeftHand.controllerTransform.position,
-                        GTPlayer.Instance.LeftHand.controllerTransform.rotation);
-
-                    if (!_platformSteam)
-                    {
-                        _leftHand.transform.Rotate(90, 0, 0);
-                        _rightHand.transform.Rotate(90, 0, 0);
-                    }
-
-                    _menu.CheckDistance();
-                    _menu.CheckReportSubmit();
-                } else if (Input.Activated && !_showingMenu)
-                {
-                    _menu.gameObject.SetActive(true);
-                    _menu.enabled = true;
-
-                    _menu.StartOverlay();
-                }
-            }
-
-            _showingMenu = _menu.gameObject.activeInHierarchy;
-
-            if ((_menu.closeButton.selected || _menu.closeButton.testPress) && _showingMenu)
-                _menu.Teardown();
+	private bool _inReportMenu;
+    public bool InReportMenu {
+        get => _inReportMenu;
+        internal set {
+            GTPlayer.Instance.InReportMenu = value;
+            _inReportMenu = value;
         }
     }
+
+    void Start() {
+        Instance = this;
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Info.Metadata.GUID);
+        GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
+
+		// NOTE: Still deciding between two different options
+		// relating to faking quest in competitive scenes for the new version:
+		// 
+		// 	A). Change the value of InReportMenu to hide hands, network the fact that
+		//      OculusReportMenu is installed to prevent competitive players from
+		//      cheating.
+		//  B). Change the value of InReportMenu to hide hands, don't network the
+		//      mod's existance, comp players have to deal with it.
+		//  C). Don't hide hands, don't network stuff, so comp players get what
+		//      they want. 
+    }
+
+    void OnPlayerSpawned() {
+        Initialized = true;
+    }
+
+	void LateUpdate() {
+		if (!Initialized) return;
+	}
+
+    public static T Load<T>(string path, string name) where T : Object
+	{
+		var ab = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(path));
+		var obj = ab.LoadAsset<T>(name);
+		
+		if (obj.Uninitialized())
+			Debug.LogError($"Cannot load assetbundle \"{path}\" object \"{name}\" to type \"{typeof(T).FullName}.\nValid streams: \n\t{Assembly.GetExecutingAssembly().GetManifestResourceNames().Join("\n\t")}");
+			
+		ab.Unload(false);
+
+		return obj;
+	}
 }
--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # OculusReportMenu 3.0
 The goal of OculusReportMenu 3.0 is to make it less reliant on the constantly changing GorillaMetaReport class by creating our own leaderboard to report with. This is an experimental idea I had, don't expect this to be implemented.
 
+```cs
+/*
+ * NOTE: I have the mod currently laid out how I want it, but it is still not functional (yet).
+ *	 The rest will come later, be patient
+*/
+```
+
+```cs
+// NOTE: Still deciding between two different options
+// relating to faking quest in competitive scenes for the new version:
+// 
+// 	A). Change the value of InReportMenu to hide hands, network the fact that
+//      OculusReportMenu is installed to prevent competitive players from
+//      cheating.
+//  B). Change the value of InReportMenu to hide hands, don't network the
+//      mod's existance, comp players have to deal with it.
+//  C). Don't hide hands, don't network stuff, so comp players get what
+//      they want. 
+```
+
 ## To-do
 - [ ] Build an Asset Bundle to start the leaderboard with, ideally, a big board simular to how it currently is
 - [ ] [Create a `MonoBehaviour` to mimic GorillaMetaReport](#important-functionality-to-implement)
