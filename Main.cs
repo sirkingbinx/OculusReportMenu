5924e78245c3ed5b8bfe34a354efcd10988bc16d
SirKingBinx <bendawsdev@gmail.com>
2026-03-20 12:55:18 -0500

    Refactor & commented on a bulk of the code hopefully people who want to learn to mod read this, and it explains how the mod works block by block so they understand it. Inspired by a certain conversation in the modding server the other day about why new modders hate learning to code mods

--- Main.cs
+++ Main.cs
@@ -1,150 +1,208 @@
-// OculusReportMenu
-// (C) Copyright 2024 - 2026 SirKingBinx (Bingus)
-// MIT License
+// OculusReportMenu/Main.cs - The Main class, that handles ORM-related stuff
+// (C) Copyright 2024 - 2026 SirKingBinx - MIT License
 
 using GorillaLocomotion;
 using GorillaNetworking;
 using System;
 
 #if MELONLOADER
+/*
+ * MelonLoader also contains some extra dependencies that we need here, all of them are
+ * just for allowing custom configurations.
+ */
 using System.IO;
 using MelonLoader;
 #endif
 
-
 using UnityEngine;
 
-namespace OculusReportMenu
+namespace OculusReportMenu;
+
+/*
+ * Here is the bulk of the code that makes reporting work, in our Main class.
+ * This handles opening/closing the report menu, updating controller input,
+ * loading config, and much more.
+ */
+internal class Main : MonoBehaviour
 {
-    internal class Main : MonoBehaviour
-    {
-        public static Main Instance;
+    public static Main? Instance;
 
-        public static GorillaMetaReport _menu;
+    public static GorillaMetaReport _menu;
 
-        private static bool __menuInit = false;
-        public static bool _menuInit {
-            get {
-                if (!__menuInit)
-                    __menuInit = (_menu != null);
-                
-                return __menuInit;
-            }
+    /*
+     * This variable tells OculusReportMenu when to start updating itself.
+     * If the GorillaMetaReport system isn't initialized, then we aren't needed
+     * and can halt ourselves until it is.
+     */
+    public static bool _menuInit {
+        get {
+            if (!field)
+                field = (_menu != null);
             
-            set => __menuInit = value; // just in case
+            return field;
         }
-        
-        private GameObject _occluder, _leftHand, _rightHand;
-        public bool _platformSteam;
+    }
+    
+    /*
+     * These are saved for faster updating.
+     * The report menu's hands, the black background (called the occluder),
+     * and if you're on Steam or not (since custom input code is required if you are)
+     */
+    private GameObject _occluder, _leftHand, _rightHand;
+    public bool _platformSteam;
 
-        public bool _showingMenu => _menu.gameObject.activeInHierarchy;
+    /*
+     * This is just a shortcut for us, not really required.
+     */
+    public bool _showingMenu => _menu.gameObject.activeInHierarchy;
 
-        internal void Start() {
-            Instance = this;
-            HarmonyLib.Harmony.CreateAndPatchAll(GetType().Assembly, Constants.Guid);
+    internal void Start() {
+        Instance = this;
 
 #if MELONLOADER
-            var configCategory = MelonPreferences.CreateCategory("Keybinds");
-            configCategory.SetFilePath($"UserData{Path.DirectorySeparatorChar}OculusReportMenu.cfg", true);
+        /*
+         * MelonLoader config is set up here.
+         * We specify a custom path so it's easy to find it.
+         */
+        var configCategory = MelonPreferences.CreateCategory("Keybinds");
+        configCategory.SetFilePath($"UserData{Path.DirectorySeparatorChar}OculusReportMenu.cfg", true);
 
-            Input.OpenButton1 = configCategory.CreateEntry("OpenButton1", ORM_Button.LeftSecondary,
-                description: "Button you use to open the report menu").Value;
-            Input.OpenButton2 = configCategory.CreateEntry("OpenButton2", ORM_Button.RightSecondary,
-                description: "Button you use to open the report menu").Value;
+        Input.OpenButton1 = configCategory.CreateEntry("OpenButton1", ORM_Button.LeftSecondary,
+            description: "Button you use to open the report menu").Value;
+        Input.OpenButton2 = configCategory.CreateEntry("OpenButton2", ORM_Button.RightSecondary,
+            description: "Button you use to open the report menu").Value;
 
-            Input.Sensitivity = configCategory.CreateEntry("Sensitivity", 0.5f,
-                description: "Sensitivity of trigger / grip detection (0.5f = 50%)").Value;
+        Input.Sensitivity = configCategory.CreateEntry("Sensitivity", 0.5f,
+            description: "Sensitivity of trigger / grip detection (0.5f = 50%)").Value;
 
-            Input.EnableTabOpening =
-                configCategory.CreateEntry("AllowTabOpen", false, description: "Press TAB to open the report menu").Value;
+        Input.EnableTabOpening =
+            configCategory.CreateEntry("AllowTabOpen", false, description: "Press TAB to open the report menu").Value;
 
-            configCategory.SaveToFile();
+        configCategory.SaveToFile();
 #elif BEPINEX
-            Input.UseCustomKeybinds = BepPlugin.Instance.Config.Bind("Keybinds",
-                "UseCustomKeybinds", true,
-                "Use your custom keybind settings (when off, press left + right secondaries)"
-            ).Value;
+        /*
+         * Same thing as above, but for BepInEx.
+         */
+        HarmonyLib.Harmony.CreateAndPatchAll(GetType().Assembly, Constants.Guid);
 
-            Input.EnableTabOpening = BepPlugin.Instance.Config.Bind("Keybinds", "AllowTabOpen", false, "Press TAB to open").Value;
+        Input.UseCustomKeybinds = Plugin.Instance.Config.Bind("Keybinds",
+            "UseCustomKeybinds", true,
+            "Use your custom keybind settings (when off, press left + right secondaries)"
+        ).Value;
 
-            Input.OpenButton1 = BepPlugin.Instance.Config.Bind("Keybinds", "OpenButton1", ORM_Button.LeftSecondary,
-                "Button you use to open the report menu").Value;
-            Input.OpenButton2 = BepPlugin.Instance.Config.Bind("Keybinds", "OpenButton2", ORM_Button.RightSecondary,
-                "Button you use to open the report menu").Value;
-            Input.Sensitivity = BepPlugin.Instance.Config.Bind("Keybinds", "Sensitivity", 0.5f,
-                "Sensitivity of trigger / grip detection (0.5f = 50%)").Value;
+        Input.EnableTabOpening = Plugin.Instance.Config.Bind("Keybinds", "AllowTabOpen", false, "Press TAB to open").Value;
+
+        Input.OpenButton1 = Plugin.Instance.Config.Bind("Keybinds", "OpenButton1", ORM_Button.LeftSecondary,
+            "Button you use to open the report menu").Value;
+        Input.OpenButton2 = Plugin.Instance.Config.Bind("Keybinds", "OpenButton2", ORM_Button.RightSecondary,
+            "Button you use to open the report menu").Value;
+        Input.Sensitivity = Plugin.Instance.Config.Bind("Keybinds", "Sensitivity", 0.5f,
+            "Sensitivity of trigger / grip detection (0.5f = 50%)").Value;
 #endif
-            GorillaTagger.OnPlayerSpawned(delegate
+        GorillaTagger.OnPlayerSpawned(delegate
+        {
+            /*
+             * 99% of the time, this code doesn't require a try {} block, but we do anyway just in case.
+             * This makes it so other mods will load properly, even if ours doesn't.
+             */
+            try {
+                _occluder = GameObject.Find("Miscellaneous Scripts/MetaReporting/ReportOccluder");
+                _leftHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/LeftHandParent");
+                _rightHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/RightHandParent");
+                
+                _platformSteam = PlayFabAuthenticator.instance.platform.PlatformTag.ToLower().Contains("steam");
+            } catch (Exception ex) {
+                Debug.Log($"Failed to load OculusReportMenu: ${ex}");
+            }
+        });
+    }
+
+    /*
+     * This variable stops the menu opening and closing every single frame your button is held down.
+     * This holds the last (updated) state of your controller inputs, so when you release the buttons
+     * it will allow you to press them again, allowing for a toggle button.
+     */
+    private bool inputActivatedBefore;
+
+    internal void Update() {
+        if (!_menuInit)
+            return; // GorillaMetaReport isn't alive yet, keep waiting
+
+        if (_menu.blockButtonsUntilTimestamp > Time.time)
+            return; // Waiting until buttons can be pressed again to update stuff
+
+        if (_showingMenu) {
+            /*
+             * Make us in the report menu and in an overlay, which turns off movement and
+             * stops our hands from colliding, so we don't hit or tag anybody while reporting.
+             */
+            GTPlayer.Instance.InReportMenu = true;
+            GTPlayer.Instance.inOverlay = true;
+
+            /*
+             * Since the menu doesn't update itself, here is where we do that.
+             * In the next 18 lines, we:
+             * - update positions of the background (occluder) and your hands,
+             * - fix the distance of the report menu so you can always reach it
+             * - submit reports if you are done reporting stuff
+             */
+            _occluder.transform.position = GorillaTagger.Instance.mainCamera.transform.position;
+
+            _rightHand.transform.SetPositionAndRotation(
+                GorillaTagger.Instance.rightHandTransform.position,
+                GorillaTagger.Instance.rightHandTransform.rotation);
+
+            _leftHand.transform.SetPositionAndRotation(
+                GorillaTagger.Instance.leftHandTransform.position,
+                GorillaTagger.Instance.leftHandTransform.rotation);
+
+            if (!_platformSteam)
             {
-                try {
-                    _occluder = GameObject.Find("Miscellaneous Scripts/MetaReporting/ReportOccluder");
-                    _leftHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/LeftHandParent");
-                    _rightHand = GameObject.Find("Miscellaneous Scripts/MetaReporting/CollisionRB/RightHandParent");
-                    
-                    _platformSteam = PlayFabAuthenticator.instance.platform.PlatformTag.ToLower().Contains("steam");
-                } catch (Exception ex) {
-                    Debug.Log($"Failed to load OculusReportMenu: ${ex}");
-                }
-            });
+                _leftHand.transform.Rotate(90, 0, 0);
+                _rightHand.transform.Rotate(90, 0, 0);
+            }
+
+            _menu.CheckDistance();
+            _menu.CheckReportSubmit();
+        } else if (!_showingMenu && !inputActivatedBefore && Input.Activated)
+        {
+            /*
+             * Here's where the menu gets activated.
+             */
+            inputActivatedBefore = true;
+            _menu.gameObject.SetActive(true);
+            _menu.enabled = true;
+
+            _menu.StartOverlay();
         }
 
-        private bool inputActivatedBefore;
+        // This `if` block resets "inputActivatedBefore" so you can press the buttons again.
+        if (inputActivatedBefore && !Input.Activated)
+            inputActivatedBefore = false;
+        
+        
+        // If the menu close button is being activated, close the menu
+        if (_showingMenu && (_menu.closeButton.selected || _menu.closeButton.testPress))
+            goto teardown;
 
-        internal void Update() {
-            if (_menuInit)
-            {
-                if (_menu.blockButtonsUntilTimestamp > Time.time)
-                    return;
+        // If you pressed your activation buttons again on the controller, close the menu
+        if (!inputActivatedBefore && Input.Activated)
+            goto teardown;
 
-                if (_showingMenu) {
-                    GTPlayer.Instance.InReportMenu = false;
-                    GTPlayer.Instance.inOverlay = true;
+        return; // Stop us from reaching the "teardown" block if we don't want to close the menu yet
 
-                    _occluder.transform.position = GorillaTagger.Instance.mainCamera.transform.position;
+        /*
+        * The code only reaches this `teardown` block when the menu is being closed to save code.
+        * It skips the "return" statement above, don't worry.
+        * This will let you move again and close the menu so your game returns.
+        */
+    teardown:
+        _menu.Teardown();
+        GTPlayer.Instance.InReportMenu = false;
+        GTPlayer.Instance.inOverlay = false;
 
-                    _rightHand.transform.SetPositionAndRotation(
-                        GorillaTagger.Instance.rightHandTransform.position,
-                        GorillaTagger.Instance.rightHandTransform.rotation);
+        inputActivatedBefore = true;
 
-                    _leftHand.transform.SetPositionAndRotation(
-                        GorillaTagger.Instance.leftHandTransform.position,
-                        GorillaTagger.Instance.leftHandTransform.rotation);
-
-                    if (!_platformSteam)
-                    {
-                        _leftHand.transform.Rotate(90, 0, 0);
-                        _rightHand.transform.Rotate(90, 0, 0);
-                    }
-
-                    _menu.CheckDistance();
-                    _menu.CheckReportSubmit();
-                } else if (!_showingMenu && !inputActivatedBefore && Input.Activated)
-                {
-                    inputActivatedBefore = true;
-                    _menu.gameObject.SetActive(true);
-                    _menu.enabled = true;
-
-                    _menu.StartOverlay();
-                }
-
-                if (inputActivatedBefore && !Input.Activated)
-                    inputActivatedBefore = false;
-            }
-
-            // these logics are seperated to save the eyes of whoever ends up needing to edit the code
-            //                                                          (that's you)
-            if (_showingMenu && (_menu.closeButton.selected || _menu.closeButton.testPress))
-            {
-                _menu.Teardown();
-                inputActivatedBefore = true;
-            }
-
-            // toggle the menu off by pressing the buttons again
-            if (!inputActivatedBefore && Input.Activated)
-            {
-                _menu.Teardown();
-                inputActivatedBefore = true;
-            }
-        }
+        return;
     }
 }
--- Patches.cs
+++ Patches.cs
@@ -1,22 +1,44 @@
-// OculusReportMenu
-// (C) Copyright 2024 - 2026 SirKingBinx (Bingus)
-// MIT License
+// OculusReportMenu/Patches.cs - Harmony Patches go here
+// (C) Copyright 2024 - 2026 SirKingBinx - MIT License
 
 using GorillaLocomotion;
 using HarmonyLib;
 
-namespace OculusReportMenu {
-    internal class Patches {
-        [HarmonyPatch(typeof(GorillaMetaReport), "Update")]
-        internal class OnOculusUpdate
-        {
-            static void Postfix() => GTPlayer.Instance.InReportMenu = false;
-        }
+namespace OculusReportMenu;
 
-        [HarmonyPatch(typeof(GorillaMetaReport), "Start")]
-        internal class OnReportInit
-        {
-            static void Postfix(GorillaMetaReport __instance) => Main._menu = __instance;
-        }
+/*
+ * Here is where Harmony Patches are stored. Harmony Patches let you call other method before, after,
+ * or during other methods being called. This is useful for many use cases:
+ * 
+ * - You want to update something with the game loop so it runs smoother
+ * - You want to update some variables passed to a method to get your own funny results
+ * - In our case, You want to completely cancel a method call so you can do stuff yourself. (See OnMetaReportUpdate)
+ */
+internal class Patches {
+    [HarmonyPatch(typeof(GorillaMetaReport), nameof(GorillaMetaReport.Update))]
+    internal class OnMetaReportUpdate
+    {
+        /* 
+         * We stop the report menu from updating itself here for a very important reason:
+         *
+         * - GorillaMetaReport will check it is running on Steam if not triggered via Automod.
+         *     The solution here is to manually move the report menu ourselves and check for input
+         *     independently (without the Steam checks obviously).
+         * - This means that OculusReportMenu requires little to no updates as long as a core
+         *     feature of Gorilla Tag has changed.
+         * - It also shaves a tiny bit of clock cycles (but it isn't 1995 so that doesn't really
+         *     matter)
+         */
+        static bool Prefix(GorillaMetaReport __instance) => true; // return true to cancel the fn call
+    }
+
+    [HarmonyPatch(typeof(GorillaMetaReport), nameof(GorillaMetaReport.Start))]
+    internal class OnMetaReportInit
+    {
+        /*
+         * This matters much less than our other Harmony patch. This method will just update
+         * the main code, telling it where the GorillaMetaReport is located.
+         */
+        static void Postfix(GorillaMetaReport __instance) => Main._menu = __instance;
     }
 }
